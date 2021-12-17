e29dddbb18b77d8d46a6965c6b32d572c6109d1a
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2021-12-17 04:00:05 -0500

    added skeleton for JS file

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Create arrays for possible characters by category
+// Lowercase letters
+
+// Uppercase letters
+
+// Numeric characters
+
+// Special characters
+
+// Select length of password
+
+// Error message when password is not between 8 and 128 characters in length
+
+// Ask about lowercase
+
+// Ask about uppercase
+
+// Ask about numeric
+
+// Ask about special
+
+// Formula to make password
+
+// Validate password contents
+
+// Print password
