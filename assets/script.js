e06b8a24935041c1e1337e719a8a2e39f6a76341
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2021-12-17 23:25:55 -0500

    prompts code added and slight reformat

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,57 @@
-// Assignment Code
+// Create arrays for possible characters by category
+// Lowercase letters
+var lowLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+// Uppercase letters
+var upLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// Numeric characters
+var numChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Special characters
+var speChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '~', '`', '?', ';', ':', ',', '.', '[', ']', '{', '}', '/', '+', '=', '-'];
+
+// Select length of password
+var length = parseInt(prompt('How many characters would you like your password to contain?'));
+
+// Error message when password is not between 8 and 128 characters in length
+if(length < 8) {
+  alert('Password must have at least 8 characters.');
+  return null;
+} else if(length > 128) {
+  alert('Password cannot be longer than 128 characters.');
+  return null;
+} else {
+  length = length;
+}
+
+// Ask about lowercase
+var hasLowChars = confirm('Would you like to include lowercased characters? Click OK to confirm.');
+
+// Ask about uppercase
+var hasUpChars = confirm('Would you like to include uppercased characters? Click OK to confirm.');
+
+// Ask about numeric
+var hasNumChars = confirm('Would you like to include numerical characters? Click OK to confirm.');
+
+// Ask about special
+var hasSpeChars = confirm('Would you like to include special characters? Click OK to confirm.');
+
+// Verify that there is at least one option selected
+if(hasLowChars === false && hasUpChars === false && hasNumChars === false && hasSpeChars === false) {
+  alert('Password must contain at least one character type.');
+  return null;
+}
+
+// Formula to make password
+
+// Validate password contents
+
+// Assignment Code (starting code)
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Print password
+// Write password to the #password input (starting code)
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,32 +60,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to generate button (starting code)
 generateBtn.addEventListener("click", writePassword);
-
-// Create arrays for possible characters by category
-// Lowercase letters
-
-// Uppercase letters
-
-// Numeric characters
-
-// Special characters
-
-// Select length of password
-
-// Error message when password is not between 8 and 128 characters in length
-
-// Ask about lowercase
-
-// Ask about uppercase
-
-// Ask about numeric
-
-// Ask about special
-
-// Formula to make password
-
-// Validate password contents
-
-// Print password
